f4e838da4b138c403128d0008bcf618e80be7c19
jeffsimp88 <jdsimpson806@gmail.com>
2021-01-08 13:56:39 -0500

    file_deleted & file_added created.

--- dirwatcher.py
+++ dirwatcher.py
@@ -9,21 +9,56 @@ import sys
 import logging
 import signal
 import time
+import os
 
 
 logger = logging.getLogger(__name__)
 
 exit_flag = False
 
+line_num = 0
+files = {}
+
 
 def search_for_magic(filename, start_line, magic_string):
     """Looks for a magic string and its line provided in command."""
     logger.info(f"Looking for {magic_string}")
-    return
+    global line_num
+    with open(filename) as f:
+        for line_num, line in enumerate(f):
+            if line_num >= start_line:
+                if magic_string in line:
+                    logger.info(f"Found {magic_string} on line {line_num}")
+    return line_num + 1
+
+
+def file_added(file_list, extension):
+    """Checks if a file is added in directory"""
+    global files
+    for f in file_list:
+        if f.endswith(extension):
+            if f not in files:
+                files[f] = 0
+                logger.info(f"{f} added to directory.")
+
+
+def file_deleted(file_list, extension):
+    """Checks if file deleted in directory."""
+    global files
+    for f in list(files):
+        if f not in file_list:
+            logger.info(f"{f} deleted from directory")
+            del files[f]
 
 
 def watch_directory(path, magic_string, extension, interval):
-    # Your code here
+    """Watches a selected directory for any added/deleted files"""
+    list_files = os.listdir(path)
+    file_added(list_files, extension)
+    file_deleted(list_files, extension)
+    for f in files:
+        path = os.path.join(path, f)
+        files[f] = search_for_magic(path, files[f], magic_string)
     return
 
 
